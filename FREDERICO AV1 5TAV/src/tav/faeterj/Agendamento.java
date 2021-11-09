c81957616e81f13c0567e6325c700c0aa2faaa30
fredsauer <90940015+fredsauer@users.noreply.github.com>
2021-11-09 15:13:46 -0300

    Update Agendamento.java

--- FREDERICO AV1 5TAV/src/tav/faeterj/Agendamento.java
+++ FREDERICO AV1 5TAV/src/tav/faeterj/Agendamento.java
@@ -3,54 +3,76 @@ package tav.faeterj;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class Agendamento {
-	public int idAgendamento;
-	public int idFuncionario;
-	public String nomeMedico;
-	public String endereco;
-	public LocalDate dataAgendamento;
-	public LocalTime hora;
+public class Agendamento 
+	{
+		public int idAgendamento;
+		public int idFuncionario;
+		public String profissionalEncarregado;
+		public String endereco;
+		public LocalDate dataAgendamento;
+		public LocalTime hora;
 	
-	public int getIdAgendamento() {
+		public int getIdAgendamento() 
+		{
 		return idAgendamento;
-	}
-	public void setIdAgendamento(int idAgendamento) {
-		this.idAgendamento = idAgendamento;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-	public LocalTime getHora() {
-		return hora;
-	}
-	public void setHora(LocalTime hora) {
-		this.hora = hora;
-	}
-	public LocalDate getDataAgendamento() {
-		return dataAgendamento;
-	}
-	public void setDataAgendamento(LocalDate dataAgendamento) {
-		this.dataAgendamento = dataAgendamento;
-	}
-	public int getIdFuncionario() {
-		return idFuncionario;
-	}
-	public void setIdFuncionario(int idFuncionario) {
-		this.idFuncionario = idFuncionario;
-	}
+		}
 	
-	public String Consulta (int idAgendamento, String nomeMedico, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) {
-		return "Consulta marcada com sucesso!";
-	}
+		public void setIdAgendamento(int idAgendamento) 
+		{
+			this.idAgendamento = idAgendamento;
+		}
+		public String getEndereco() 
+		{
+			return endereco;
+		}
 	
-	public String Exame (int idAgendamento, String nomeMedico, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) {
-		return "Exame marcado com sucesso!";
-	}
+		public void setEndereco(String endereco) 
+		{
+			this.endereco = endereco;
+		}
 	
-	public String Internacao (int idAgendamento, String nomeMedico, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) {
-		return "Internação realizada!";
+		public LocalTime getHora() 
+		{
+			return hora;
+		}
+	
+		public void setHora(LocalTime hora) 
+		{
+			this.hora = hora;
+		}
+	
+		public LocalDate getDataAgendamento() 
+		{
+			return dataAgendamento;
+		}
+	
+		public void setDataAgendamento(LocalDate dataAgendamento) 
+		{
+			this.dataAgendamento = dataAgendamento;
+		}
+	
+		public int getIdFuncionario() 
+		{
+			return idFuncionario;
+		}
+	
+		public void setIdFuncionario(int idFuncionario) 
+		{
+			this.idFuncionario = idFuncionario;
+		}
+	
+		public String Consulta (int idAgendamento, String profissionalEncarregado, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) 
+		{
+			return "TIPO DE COBERTURA: CONSULTA";
+		}
+	
+		public String Exame (int idAgendamento, String profissionalEncarregado, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) 
+		{
+			return "TIPO DE COBERTURA: EXAME";
+		}
+	
+		public String Internacao (int idAgendamento, String profissionalEncarregado, String endereco, int idFuncionario, LocalTime hora, LocalDate DataAgendamento) 
+		{
+			return "TIPO DE COBERTURA: INTERNAÇÃO";
+		}
 	}
-}
